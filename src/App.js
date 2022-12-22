2a8801f47916d8236f081eceafc4f136774ec29d
Emily Santos <eyssantos@gmail.com>
2022-12-22 11:33:19 +0000

    add dummy goal

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import GoalList from './components/GoalList';
+import GoalList from './components/GoalList/GoalList';
+import NewGoal from './components/NewGoal/NewGoal';
 import './App.css';
 
 const App = () => {
@@ -9,9 +10,15 @@ const App = () => {
     {id: 'cg3', text: 'Learn Node'}
   ];
 
+  const addNewGoalHandler = (newGoal) => {
+    courseGoals.push(newGoal);
+    console.log(courseGoals);
+  }
+
   return (
     <div className='course-goals'>
       <h2>Goals for today</h2>
+      <NewGoal onAddGoal={addNewGoalHandler}/>
       <GoalList goals={courseGoals}/>
     </div>
   )
